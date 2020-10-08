2f0f6694774b0e70f73603d80ac9bbbf2c23afca
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-10-07 19:52:28 -0500

    Update README

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -6,7 +6,7 @@ mkdir -p ffmpeg_output
 
 # Set MXE environment variables
 export PATH=$(pwd)/mxe/usr/bin:$(pwd)/mxe/usr/x86_64-pc-linux-gnu/bin:$PATH
-MXE_DIR = $(pwd)/mxe/usr/x86_64-w64-mingw32.shared
+MXE_DIR=$(pwd)/mxe/usr/x86_64-w64-mingw32.shared
 
 # Download FFmpeg
 wget https://ffmpeg.org/releases/ffmpeg-$FFMPEG_VERSION.tar.gz
@@ -47,9 +47,8 @@ rm -rf usr
 mv bin/*.lib lib/
 
 # Copy all dependent DLLs
-cd bin/
-for f in *.{dll,exe}; do
-    for d in $(peldd $f); do
+copy_dependent_dlls() {
+    for d in $(peldd $1); do
         echo "$f: $d"
         if [[ -f "$(pwd)/$d" ]]; then
             echo "Library $d is already part of FFmpeg"
@@ -57,7 +56,17 @@ for f in *.{dll,exe}; do
             for l in $(find $MXE_DIR -name $d); do
                 echo "Library $d found at: $l"
                 cp $l .
+                copy_dependent_dlls $l
             done
         fi
     done
+}
+
+pushd bin/
+for f in *.{dll,exe}; do
+    copy_dependent_dlls $f
 done
+popd
+
+# Compress FFMpeg distribution
+tar -cvzf ffmpeg-$FFMPEG_VERSION-win64-shared-dev-lgpl.tar.gz *
